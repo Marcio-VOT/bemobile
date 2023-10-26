f60ba24b11c991652e110c2533f42a5a0f1b877d
Marcio-VOT <marciofsantos65@gmail.com>
2023-10-26 05:16:51 -0300

    feat: detail a product;

--- app/Controllers/Http/ProductsController.ts
+++ app/Controllers/Http/ProductsController.ts
@@ -2,6 +2,7 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Product from 'App/Models/Product'
 import CreateProductValidator from 'App/Validators/CreateProductValidator'
 import httpStatus from 'http-status'
+import { DateTime } from 'luxon'
 
 export default class ProductsController {
   public async index ({ request: _req, response: res }: HttpContextContract) {
@@ -20,6 +21,31 @@ export default class ProductsController {
     const product = await Product.create(payload)
 
     res.status(httpStatus.CREATED)
+    res.send({ product })
+  }
+
+  public async destroy ({ params: { id }, response: res }: HttpContextContract) {
+    const product = await Product.find(id)
+
+    if(product?.deleted_at !== null || !product){
+      res.status(httpStatus.NOT_FOUND)
+      return res.send({ message: 'Product not found' })
+    }
+
+    product.deleted_at = DateTime.now()
+    await product.save()
+
+    res.send({ message: 'Product deleted' })
+  }
+
+  public async show ({ params: { id }, response: res }: HttpContextContract) {
+    const product = await Product.query().whereNull('deleted_at').where('id', id).first()
+
+    if(!product){
+      res.status(httpStatus.NOT_FOUND)
+      return res.send({ message: 'Product not found' })
+    }
+
     res.send({ product })
   }
 }
