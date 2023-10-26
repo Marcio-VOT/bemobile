c9675b9e019cffc691984e3cd54011eb48e678e7
Marcio-VOT <marciofsantos65@gmail.com>
2023-10-26 14:25:54 -0300

    feat: partial user creation route;

---
+++ app/Controllers/Http/UsersController.ts
@@ -0,0 +1,11 @@
+// import { Hash } from '@adonisjs/core/build/standalone'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User'
+
+export default class UsersController {
+  public async store ({ request, response }: HttpContextContract) {
+    const data = request.only(['name', 'email', 'password'])
+    const user = await User.create(data)
+    return response.status(201).json(user)
+  }
+}
