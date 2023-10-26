44a5a81432706c6459a87c0bc826f639f59bfc64
Marcio-VOT <marciofsantos65@gmail.com>
2023-10-26 14:38:41 -0300

    feat: auth initial config;

--- database/migrations/1698185975514_users.ts
+++ database/migrations/1698185975514_users.ts
@@ -5,15 +5,16 @@ export default class extends BaseSchema {
 
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id')
-      table.string('email').unique().notNullable()
-      table.string('password').notNullable()
+      table.increments('id').primary()
+      table.string('email', 255).notNullable().unique()
+      table.string('password', 180).notNullable()
+      table.string('remember_me_token').nullable()
 
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
        */
-      table.timestamp('created_at', { useTz: true })
-      table.timestamp('updated_at', { useTz: true })
+      table.timestamp('created_at', { useTz: true }).notNullable()
+      table.timestamp('updated_at', { useTz: true }).notNullable()
     })
   }
 
