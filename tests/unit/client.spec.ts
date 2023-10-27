4d0be3ad8088f3ca78d45924e5c0cb752fe915be
Marcio-VOT <marciofsantos65@gmail.com>
2023-10-26 21:56:32 -0300

    feat(test): client update;

--- tests/unit/client.spec.ts
+++ tests/unit/client.spec.ts
@@ -1,6 +1,8 @@
-import { faker } from '@faker-js/faker'
+import { faker, faker } from '@faker-js/faker'
 import { test } from '@japa/runner'
+import Address from 'App/Models/Address'
 import Client from 'App/Models/Client'
+import Phone from 'App/Models/Phone'
 import User from 'App/Models/User'
 import httpStatus from 'http-status'
 
@@ -39,17 +41,44 @@ test.group('Client', async () => {
 
     (await response.loginAs(user)).assertStatus(httpStatus.OK)
   })
-
+  const clientO = await Client.create({
+    name: faker.person.firstName(),
+    cpf: `${faker.number.int({max: 99_999_999_999, min: 10_000_000_000})}`,
+  })
+  await Phone.create({
+    phone: `${faker.number.int({max: 99_999_999_999, min: 10_000_000_000})}`,
+    client_id: clientO.id,
+  })
+  await Address.create({
+    zip_code: '83410300',
+    country: 'Brazil',
+    state: 'Paraná',
+    city: 'Colombo',
+    street: 'Jaguariaíva',
+    neighborhood: 'Paloma',
+    number:875,
+    client_id: clientO.id,
+    complement: 'Casa',
+  })
   test('should show a client', async ({client}) => {
-    const clientO = await Client.create({
-      name: faker.person.firstName(),
-      cpf: `${faker.number.int({max: 99_999_999_999, min: 10_000_000_000})}`,
-    })
-
     const response = client.get(`/api/clients/${clientO.id}`).guard('api');
 
     (await response.loginAs(user)).assertStatus(httpStatus.OK)
   }
   )
+
+  test('should update a client', async ({client}) => {
+    const response = client.patch(`/api/clients/${clientO.id}`)
+      .form({
+        client:{
+          name: faker.person.firstName(),
+          cpf: `${faker.number.int({max: 99_999_999_999, min: 10_000_000_000})}`,
+        },
+      }
+      ).guard('api')
+    ;
+
+    (await response.loginAs(user)).assertStatus(httpStatus.OK)
+  })
 })
 
